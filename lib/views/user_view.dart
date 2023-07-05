2409024c239a7ee1171f45912f2ef2bc08463824
3khaled3 <ff.tark123@gmail.com>
2023-07-05 03:44:29 +0300

    refactorProfileScreen

--- lib/views/user_view.dart
+++ lib/views/user_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, non_constant_identifier_names
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wechat/views/appinfo_view.dart';
@@ -18,82 +18,102 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
         body: Container(
       height: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFF183E36), Colors.black, Color(0xFF183E36)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          transform: GradientRotation(80),
-        ),
-      ),
+      decoration: _BuildBackGround(),
       child: SafeArea(
           child: SingleChildScrollView(
         child: Column(
           children: [
             const UserInformation(),
             const Divider(),
-            CustomElevatedButton(
-              title: 'Edit Profile',
-              icon: Icons.edit,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const EditProfile();
-                  },
-                ));
-              },
-            ),
+            _BuildEditProfileButtom(context),
             const Divider(),
-            CustomElevatedButton(
-              title: 'Change Password',
-              icon: Icons.lock,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const ChangePassword();
-                  },
-                ));
-                // Navigate to the change password screen
-              },
-            ),
+            _BuildChangePassButtom(context),
             const Divider(),
-            CustomElevatedButton(
-              title: 'App info',
-              icon: Icons.info_outline,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const appinfo();
-                  },
-                ));
-              },
-            ),
+            _BuildAppInfoButtom(context),
             const Divider(),
             BlocBuilder<AuthCubit, AuthState>(builder: (context, state) {
-              return CustomElevatedButton(
-                title: 'Logout',
-                icon: Icons.logout,
-                onPressed: () async {
-                  await BlocProvider.of<AuthCubit>(context).signOut();
-                  final state = BlocProvider.of<AuthCubit>(context).state;
-                  if (state is success) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoginScreen(),
-                      ),
-                    );
-                  } else if (state is error) {
-                    final errorMessage = (state).errorMessage;
-                    showSnackbarMessage(context, errorMessage, Colors.red);
-                  }
-                  // Navigate to the change password screen
-                },
-              );
+              return _BuildLogOutButtom(context);
             })
           ],
         ),
       )),
     ));
+  }
+
+  CustomElevatedButton _BuildLogOutButtom(BuildContext context) {
+    return CustomElevatedButton(
+              title: 'Logout',
+              icon: Icons.logout,
+              onPressed: () async {
+                await BlocProvider.of<AuthCubit>(context).signOut();
+                final state = BlocProvider.of<AuthCubit>(context).state;
+                if (state is success) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                } else if (state is error) {
+                  final errorMessage = (state).errorMessage;
+                  showSnackbarMessage(context, errorMessage, Colors.red);
+                }
+                // Navigate to the change password screen
+              },
+            );
+  }
+
+  CustomElevatedButton _BuildAppInfoButtom(BuildContext context) {
+    return CustomElevatedButton(
+            title: 'App info',
+            icon: Icons.info_outline,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const appinfo();
+                },
+              ));
+            },
+          );
+  }
+
+  CustomElevatedButton _BuildChangePassButtom(BuildContext context) {
+    return CustomElevatedButton(
+            title: 'Change Password',
+            icon: Icons.lock,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const ChangePassword();
+                },
+              ));
+              // Navigate to the change password screen
+            },
+          );
+  }
+
+  CustomElevatedButton _BuildEditProfileButtom(BuildContext context) {
+    return CustomElevatedButton(
+            title: 'Edit Profile',
+            icon: Icons.edit,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const EditProfile();
+                },
+              ));
+            },
+          );
+  }
+
+  BoxDecoration _BuildBackGround() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Color(0xFF183E36), Colors.black, Color(0xFF183E36)],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        transform: GradientRotation(80),
+      ),
+    );
   }
 }
