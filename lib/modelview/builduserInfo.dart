8721d7bd8c229a04f0a1a9dfb995a58456797f34
3khaled3 <ff.tark123@gmail.com>
2023-07-12 09:47:19 +0300

    CachedNetworkImage1

--- lib/modelview/builduserInfo.dart
+++ lib/modelview/builduserInfo.dart
@@ -6,7 +6,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import '../cubits/auth_cubit/auth_cubit.dart';
 
 class UserInformation extends StatefulWidget {
